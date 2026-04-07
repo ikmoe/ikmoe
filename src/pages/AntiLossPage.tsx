a58a2027ccf4611c2dfa382f87f096e182ceb04b
ikmoe <k@005.ink>
2026-04-07 16:32:38 +0800

    更新 AntiLossPage.tsx

--- src/pages/AntiLossPage.tsx
+++ src/pages/AntiLossPage.tsx
@@ -150,8 +150,6 @@ const QRPlaceholder = ({ label }: { label: string }) => (
   </div>
 );
 
-<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=298 height=52 src="//music.163.com/outchain/player?type=2&id=2619637167&auto=1&height=32"></iframe>
-
 /* ─── Channel Card ─── */
 
 interface ChannelCardProps {
