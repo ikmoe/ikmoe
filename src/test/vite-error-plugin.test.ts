422a2e3953409ff9928210fea479cae72f152daa
ikmoe <k@005.ink>
2026-04-12 23:15:26 +0800

    标题

--- src/test/vite-error-plugin.test.ts
+++ src/test/vite-error-plugin.test.ts
@@ -55,7 +55,7 @@ describe("vite-error-plugin: transformIndexHtml", () => {
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>长空通讯防失联页</title>
+    <title>长空通信</title>
   </head>
   <body>
     <div id="root"></div>
